8c9c3b0ac6c7eb2202b4bb771e2f8bfb9b375931
yahyamouman <56233662+yahyamouman@users.noreply.github.com>
2022-01-30 07:06:28 +0100

    added setup.sh

---
+++ setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+apt-get install python3
+apt-get install unzip
+unzip XML-Coll-withSem.zip -d XML-Coll-withSem
+apt-get install pip
+pip install numpy
+pip installl nltk
+pip install lxml
+pip install beatifulsoup4
